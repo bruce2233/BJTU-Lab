a409a98888ad22cbf76a47e87e94d158eab47eac
LAPTOP-N1195GTQ\86442 <864427717@qq.com>
2022-05-31 00:37:05 +0800

    image processing

--- image_processing/image_processing_test.go
+++ image_processing/image_processing_test.go
@@ -1,14 +1,16 @@
 package image_processing
 
 import (
+	"fmt"
+	"os"
 	"testing"
 )
 
 func TestImage(t *testing.T) {
 	image()
-	// outputFile, err := os.OpenFile("E:\\学习资料\\数字图像处理\\output.txt", os.O_RDWR, os.ModePerm)
-	// if err != nil {
-	// 	fmt.Println("open error")
-	// }
-	// fmt.Fprint(outputFile, "Fuck U!")
+	outputFile, err := os.OpenFile("E:\\学习资料\\数字图像处理\\output.txt", os.O_RDWR, os.ModePerm)
+	if err != nil {
+		fmt.Println("open error")
+	}
+	fmt.Fprint(outputFile, "Fuck U!")
 }
