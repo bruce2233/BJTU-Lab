5bde98de317f7020823c8df755a6e38d8362120d
LAPTOP-N1195GTQ\86442 <864427717@qq.com>
2022-07-07 19:49:46 +0800

    daily

--- deisgn_pattern/color/colorFactory.go
+++ deisgn_pattern/color/colorFactory.go
@@ -5,8 +5,6 @@ import (
 )
 
 func ColorStrFactory(s string, front int, background int, special int) (string, string) {
-
 	rtn := "\033[" + strconv.Itoa(front) + ";" + strconv.Itoa(background) + ";" + strconv.Itoa(special) + "m%s\033[0m"
 	return rtn, s
 }
-
